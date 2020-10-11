c365ee39e3238b5309355bb69f94b300135bb12d
hardeep.singh <hardeep.singh@infoedge.com>
2020-10-12 01:01:30 +0530

    active page disabled for onclick

--- src/componenets/Pagination.js
+++ src/componenets/Pagination.js
@@ -2,35 +2,31 @@ import React from "react";
 import "./assets/Pagination.css";
 
 const Pagination = ({ pageData, handlePagination }) => {
- 
   return (
     <ul className="pagination">
-      <li
-        className={pageData.prev === null ? "disabled" : ""}
-        onClick={() => handlePagination(pageData.prev, "prev")}
-      >
-        <span className={pageData.prev === null ? "disabled fade" : ""}>&lt;</span>
+      <li className={pageData.prev === null ? "disabled" : ""}>
+        <span
+          className={pageData.prev === null ? "disabled fade" : ""}
+          onClick={() => handlePagination(pageData.prev, "prev")}
+        >
+          &lt;
+        </span>
       </li>
       {pageData.pages.map((pg) => (
-        <li key={pg} onClick={() => handlePagination(pg)}>
+        <li key={pg}>
           <span
             className={pg === pageData.activePage ? "active" : ""}
+            onClick={() => handlePagination(pg)}
           >
             {pg}
           </span>
         </li>
       ))}
-      <li
-        className={
-          pageData.next === null ? "disabled" : ""
-        }
-        onClick={() => handlePagination(pageData.next, "next")}
-      >
+      <li className={pageData.next === null ? "disabled" : ""}>
         <span
           href="#0"
-          className={
-            pageData.next === null ? "disabled fade" : ""
-          }
+          className={pageData.next === null ? "disabled fade" : ""}
+          onClick={() => handlePagination(pageData.next, "next")}
         >
           &gt;
         </span>
